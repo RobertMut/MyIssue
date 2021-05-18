a68eea76b38f8cccd229dce9850d33d440e5b98d
RobertMut <robertmutnianski@vp.pl>
2021-05-18 18:59:37 +0200

    Moved identifying client. Added class with counter.

---
+++ MyIssue.Server/ClientProcessing/ClientCounter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIssue.Server
+{
+    public static class ClientCounter
+    {
+        public static int Clients { get; set; }
+    }
+}
---
+++ MyIssue.Server/ClientProcessing/IdentifyClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyIssue.Server
+{
+    class IdentifyClient
+    {
+        
+        public Task ConnectedTask(Socket sock, CancellationToken ct)
+        {
+            Identify(sock, ct);
+            return Task.CompletedTask;
+        }
+        private void Identify(Socket sock, CancellationToken ct)
+        {
+            try
+            {
+                Commands ac = new Commands();
+                Console.WriteLine("Connected");
+                var cli = new ClientIdentifier()
+                {
+                    ConnectedSock = sock,
+                    Id = ClientCounter.Clients++,
+                    CommandHistory = new List<string>(),
+                    Status = 0,
+                    terminated = false,
+                    loggedIn = false
+                };
+                ac.Client(cli, ct);
+                ct.ThrowIfCancellationRequested();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                sock.Close();
+
+            }
+
+        }
+    }
+}
