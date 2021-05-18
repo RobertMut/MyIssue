35b4111d96218edbd695fbbd948c0ec753f65d45
RobertMut <robertmutnianski@vp.pl>
2021-05-18 18:38:17 +0200

    Database connection with basic query to insert task.

---
+++ MyIssue.Server/Database/DBConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIssue.Server.Database
+{
+    public class DBConnector
+    {
+        public void MakeQuery(SqlConnectionStringBuilder builder, SqlCommand command)
+        {
+            using(SqlConnection conn = new SqlConnection(builder.ConnectionString))
+            {
+                conn.Open();
+                using(SqlCommand cmd = command)
+                {
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine("DONE\r\n");
+        }
+        public SqlConnectionStringBuilder SqlBuilder(DBParameters parameters)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = parameters.DBAddress;
+            builder.UserID = parameters.Username;
+            builder.Password = parameters.Password;
+            builder.InitialCatalog = parameters.Database;
+            return builder;
+        }
+    }
+}
---
+++ MyIssue.Server/Database/Queries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIssue.Server.Database
+{
+    public class Queries
+    {
+        public SqlCommand InsertNewTask(string[] values, string table) {
+            string insertQuery = "INSERT INTO "+table+"(taskTitle, taskDesc, taskCreation, taskClient, taskType)VALUES(@TITLE, @DESC, @DATE, @CLIENT, @TYPE)";
+            Tools t = new Tools();
+            using (SqlCommand cmd = new SqlCommand(insertQuery))
+            {
+                DateTime.TryParse(values[2], out DateTime date);
+                int? client = t.NullableInt(values[3]);
+                cmd.Parameters.Add("@TITLE", SqlDbType.VarChar,100).Value = values[0];
+                cmd.Parameters.Add("@DESC", SqlDbType.VarChar).Value = values[1];
+                cmd.Parameters.Add("@DATE", SqlDbType.DateTime).Value = Convert.ToDateTime(values[2]);
+                cmd.Parameters.Add("@CLIENT", SqlDbType.Decimal).Value = client == null ? (object)DBNull.Value : t.NullableInt(values[3]);
+                cmd.Parameters.Add("@TYPE", SqlDbType.Decimal).Value = Convert.ToDecimal(values[4]);
+                return cmd;
+            }
+
+        }
+    }
+}
