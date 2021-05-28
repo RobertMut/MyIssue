b8a395409f967c72a7bd4f4fbb4f82f5a1f2e816
RobertMut <robertmutnianski@vp.pl>
2021-05-28 18:05:12 +0200

    Simplified console writing for user input.

---
+++ MyIssue.Server/ConsoleOut/LogUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIssue.Server
+{
+    public static class LogUser
+    {
+        public static void TypedCommand(string method, string message, Client c)
+        {
+            Console.WriteLine("{0} - {1} - {2} - {3} {4}", c.ConnectedSock.RemoteEndPoint, c.Id, DateTime.Now, message, method);
+        }
+    }
+}
