8d4a0997c121070429d710f11a276b4bf5591278
RobertMut <robertmutnianski@vp.pl>
2021-09-13 18:17:15 +0200

    Deleted commands employee, tasks. Added HttpService.

---
+++ Core/Interfaces/IHttpService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyIssue.Core.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<string> Get(string path);
+        Task<Uri> Post<T>(string path, T entity);
+        Task<string> Put<T>(string path, T entity);
+        Task<HttpStatusCode> Delete(string path, int id);
+    }
+}
